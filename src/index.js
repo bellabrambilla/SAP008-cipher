a58298ec3098ddeb0584f0beede74a7ffc1c4024
Isabella B <bellabrambilla@gmail.com>
2022-07-27 17:46:36 -0300

    inicialização do JS index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import cipher from './cipher.js';
 
+const codificar = document.getElementById('codificar');
+codificar.addEventListener('click', _______);
+
+const decodificar = document.getElementById('decodificar');
+decodificar.addEventListener('click', _______);
+
+const traduzir = document.getElementById('traduzir');
+traduzir.addEventListener('click', encriptar);
+
+function encriptar () {
+    let codificaArea = document.getElementById('codificaArea').value;
+    let chave = document.getElementById('chave').value;
+
+if (codificaArea == "" || chave == "") {
+    alert ("Você deve escrever sua mensagem e escolher a chave de codificação.");
+
+} else {
+     
+
+
+}
+
+
+
+
+
+}
+
+
+
+
+
 console.log(cipher);
